361817428271cefda6a2c4cd543ccee63ad38132
bakhtiyargil <bext97@mail.ru>
2024-01-30 23:50:07 +0400

    grpc protocol partially implemented

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "ka-cache/grpc"
+	"log"
+	"time"
+)
+
+func main() {
+	var optsDial []grpc.DialOption
+
+	optsDial = append(optsDial, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial("localhost:5000", optsDial...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewCacheClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	response, err := client.Put(ctx, &pb.Item{
+		Key:   "Isaq",
+		Value: "A",
+	})
+
+	fmt.Println(response)
+}
--- main/main.go
+++ main/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"ka-cache/api/server"
-	"ka-cache/config"
+	"fmt"
+	"google.golang.org/grpc"
+	pb "ka-cache/grpc"
+	"ka-cache/grpc/server"
+	"log"
+	"net"
 )
 
 func main() {
-	lConfig, _ := config.LoadConfig("./config/config-local")
-	eServer := server.NewServer(lConfig)
-	err := eServer.Run()
+	/*	lConfig, _ := config.LoadConfig("./config/config-local")
+		eServer := server.NewServer(lConfig)
+		err := eServer.Run()
+		if err != nil {
+			return
+		}*/
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 5000))
 	if err != nil {
-		return
+		log.Fatalf("failed to listen: %v", err)
 	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterCacheServer(grpcServer, server.NewServer())
+	grpcServer.Serve(lis)
 }
