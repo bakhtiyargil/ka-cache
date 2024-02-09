ab4d83fbc8ad7d8cab9ef064d7ae274ea3c0a28e
bakhtiyargil <bext97@mail.ru>
2024-02-09 17:25:13 +0400

    errors corrected

--- model/object.go
+++ model/object.go
@@ -4,3 +4,12 @@ type Item struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
+
+type SuccessResponse struct {
+	Message string `json:"message"`
+}
+
+func NewSuccessResponse() *SuccessResponse {
+	resp := SuccessResponse{Message: "success"}
+	return &resp
+}
