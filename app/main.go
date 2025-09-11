2913302082f469a048993aad319d0644499550a9
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-09-11 11:16:38 +0400

    notify channel moved to method

--- app/main.go
+++ app/main.go
@@ -12,22 +12,21 @@ import (
 )
 
 func main() {
-	stopChan := make(chan os.Signal, 1)
-	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
-
 	c := cache.NewLruCache[string, string](bootstrap.App.Config.Cache.Capacity, bootstrap.App.Logger)
 	go c.StartCleanup(bootstrap.App.Config.Cache.CleanupInterval * time.Second)
-
-	startServers(c, stopChan)
+	startServers(c)
 }
 
-func startServers(cache cache.Cache[string, string], stopChan chan os.Signal) {
+func startServers(cache cache.Cache[string, string]) {
 	h := http.NewCacheHandler(cache)
 	hServer := http.NewHttpServer(bootstrap.App.Config, bootstrap.App.Logger, h)
 	hServer.Start()
 
 	gServer := grpc.NewGrpcServer(bootstrap.App.Config, bootstrap.App.Logger, cache)
 	gServer.Start()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
 
 	select {
 	case <-stopChan:
